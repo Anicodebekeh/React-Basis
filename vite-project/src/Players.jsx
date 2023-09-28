6b5cce42b9fc9fb005d03f5bf9f1bca9fd93c7f2
Anicodebekeh <102191636+Anicodebekeh@users.noreply.github.com>
2023-09-29 00:27:55 +0100

    working with object in state restructure by copying the state

--- vite-project/src/Players.jsx
+++ vite-project/src/Players.jsx
@@ -3,17 +3,25 @@ import { useState } from "react"
 export default function Player(){
     const [score, setScore]= useState({p1score:0, p2score:0})
     
-    const incrementScore =()=>{
-        const newScore={...score, p1score:score.p1score+=1}
-        setScore(newScore)
+    const incrementScorep1 =()=>{
+        setScore((oldscore)=>{
+           return { ...oldscore, p1score: oldscore.p1score+1}
+        })
+    }
+
+    const incrementScorep2 =()=>{
+        setScore((oldscore)=>{
+           return { ...oldscore, p2score: oldscore.p2score+1}
+        })
     }
 
     return(
         <div>
             <p> player1: {score.p1score}</p>
             <p> player2: {score.p2score}</p>
-            <button onClick={incrementScore}>p1score</button>
-            <button>p2score</button>
+            <button onClick={incrementScorep1}>p1score</button>
+            <button onClick={incrementScorep2}>p1score</button>
+            
 
         </div>
     )
