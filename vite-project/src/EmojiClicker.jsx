310270ca4e77cd4cbeedb5e0d5e23b3207cad539
Anicodebekeh <102191636+Anicodebekeh@users.noreply.github.com>
2023-09-29 03:08:04 +0100

    helper function that adds in random emoji

--- vite-project/src/EmojiClicker.jsx
+++ vite-project/src/EmojiClicker.jsx
@@ -1,12 +1,23 @@
 import uuid from 'react-uuid';
 import { useState } from "react";
 
-
+const choice = ['🍾','😊', '😢', '😊', '😿']
+    const randomEmojis = ()=>{
+        const idx =Math.floor(Math.random()*choice.length)
+        return choice[idx]
+    }
 export default function EmojiCliker(){
-    const [emoji, setEmoji]= useState([{id: uuid(), emoji:'😊'}])
+    const [emoji, setEmoji]= useState([{id: uuid(), emoji: randomEmojis()}])
 
    const addEmoji = ()=>{
-    setEmoji(oldEmojis => [...oldEmojis, {id:uuid(), emoji:'😢'}])
+    setEmoji(oldEmojis => [...oldEmojis, {id:uuid(), emoji:randomEmojis()}])
+   }
+
+   const deleteEmoji = (id)=>{
+    setEmoji(oldEmojis => {
+       return  oldEmojis.filter(e => e.id !==id)
+    })
+    console.log(id)
    }
 
     // const newEmoji= setEmoji([...emoji, '😢'])
@@ -14,7 +25,7 @@ export default function EmojiCliker(){
     return(
         <div>
             {emoji.map((e)=> (
-             <span key={e.id}>{e.emoji}</span>
+             <span key={e.id} onClick={()=> deleteEmoji(e.id)}>{e.emoji}</span>
             ))}
             <button onClick={addEmoji}>add emoji</button>
         </div>
