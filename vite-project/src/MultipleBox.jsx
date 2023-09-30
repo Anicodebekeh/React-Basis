23faa8aa0ba44478e5ecc9a85138b94c1827b12e
Anicodebekeh <102191636+Anicodebekeh@users.noreply.github.com>
2023-09-30 23:46:17 +0100

    player component done with the button

--- vite-project/src/MultipleBox.jsx
+++ vite-project/src/MultipleBox.jsx
@@ -3,16 +3,10 @@ import './MultipleBox.css'
 
 export default function MultipleBox ({color, nums}){
     const boxes = []
-   
     for (let i=0; i<nums; i++){
-        boxes.push(<Box color={color}/>)
+        // console.log(i)
+        boxes.push( <Box color={color}/>)
     }
-
     
     return <div className="multipleBox">{boxes}</div>
 }
-
-// arraySparse.forEach((element) => {
-//   console.log({ element });
-//   numCallbackRuns++;
-// });
--- vite-project/src/Score.jsx
+++ vite-project/src/Score.jsx
@@ -2,22 +2,36 @@ import { useState } from "react";
 
 export default function score({numPlayer, target}){
     const [scores, setScore]=useState(new Array(numPlayer).fill(0))
-
     const addOne = (idx)=>{
         setScore((prevScore) =>{
             return prevScore.map((score, i)=>{
-                if (i===idx) return score+1;
+                if (i===idx) {
+                    return score+1;
+                }
                 return score
+                
             }) 
         })
     }
+
+    const reset=()=>{
+        setScore(new Array(numPlayer).fill(0))
+    }
+  
 
     return (
         <div>
             <h1>Score Keeper</h1>
            <ul> {scores.map((score, idx)=>{
-                return <li key={idx}>player {idx+1} : {score} <button onClick={()=> addOne(idx)}>+1</button></li>
+                return (
+                    <div>
+                        <li key={idx}>player {idx+1} : {score} <button onClick={()=> addOne(idx)}  disabled={score>=target}>+1</button></li>
+                        { score>=target&& 'Winner'}
+                    </div>
+                )
             })}</ul>
+
+            <button onClick={reset}>Reset</button>
         </div>
     )
 }
